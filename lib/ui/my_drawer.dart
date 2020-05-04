bcdfe59a79127f480627cde2260f23c2c56de1f1
Chris Fung <chris.fung2@gmail.com>
2020-05-03 22:24:45 -0700

    Drawer no longer needs a future builder

--- lib/ui/my_drawer.dart
+++ lib/ui/my_drawer.dart
@@ -1,7 +1,5 @@
-
 import 'package:flex/models/user.dart';
 import 'package:flex/provider_notifiers/drawer_notifier.dart';
-import 'package:flex/screens/loading.dart';
 import 'package:flex/services/auth.dart';
 import 'package:flex/wrappers/auth_wrapper.dart';
 import 'package:flutter/material.dart';
@@ -15,6 +13,7 @@ class MyDrawer extends StatelessWidget {
     final drawerNotifier =
     Provider.of<DrawerStateNotifier>(context, listen: false);
     final user = Provider.of<User>(context);
+    final displayName = Provider.of<String>(context);
     String userName;
 
     if (user == null) {
@@ -22,116 +21,82 @@ class MyDrawer extends StatelessWidget {
     } else if (user.userInfo.isAnonymous) {
       userName = 'Anonymous';
     } else {
-      userName = user.userInfo.displayName;
+      userName = displayName;
     }
 
     return SafeArea(
-      child: userName == null
-          ? _buildFutureDrawer(drawerNotifier)
-          : _buildDrawer(context, userName, drawerNotifier),
-    );
-  }
-
-  //if user data hasn't updated locally, ask Firebase Auth for the current user data
-  Widget _buildFutureDrawer(DrawerStateNotifier drawerNotifier) {
-    return FutureBuilder<String>(
-      future: _auth.getDisplayName(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          if (snapshot.hasError || snapshot.data == null) {
-            return _buildDrawer(context, 'Not signed in', drawerNotifier);
-          } else {
-            return _buildDrawer(
-                context, snapshot.data, drawerNotifier);
-          }
-        } else {
-          return Column(
-            children: <Widget>[
-              Expanded(
-                child: Center(
-                  child: Loading(),
-                ),
-              ),
-            ],
-          );
-        }
-      },
-    );
-  }
-
-  Widget _buildDrawer(BuildContext context, String userName,
-      DrawerStateNotifier drawerNotifier) {
-    return Drawer(
-      child: ListView(
-        children: <Widget>[
+      child: Drawer(
+        child: ListView(
+          children: <Widget>[
 //            UserAccountsDrawerHeader(
 //              accountName: Text('chris'),
 //              accountEmail: Text(''),
 //            ),
-          DrawerHeader(
-            decoration: BoxDecoration(color: Theme
-                .of(context)
-                .primaryColor),
-            child: Align(
-              alignment: Alignment.bottomLeft,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    userName,
-                    style: Theme
-                        .of(context)
-                        .textTheme
-                        .headline6
-                        .copyWith(color: Colors.white),
-                  ),
-                  const SizedBox(height: 5.0),
-                  FlatButton(
-                    child: Text(
-                      'Sign out',
+            DrawerHeader(
+              decoration: BoxDecoration(color: Theme
+                  .of(context)
+                  .primaryColor),
+              child: Align(
+                alignment: Alignment.bottomLeft,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      userName,
                       style: Theme
                           .of(context)
                           .textTheme
-                          .subtitle2
-                          .copyWith(
-                        color: Colors.cyan[400],
-                        fontSize: 15.0,
-                      ),
+                          .headline6
+                          .copyWith(color: Colors.white),
                     ),
-                    onPressed: () async {
-                      await _auth.signOut();
-                      Navigator.of(context).pushAndRemoveUntil(
-                          MaterialPageRoute(builder: (_) => AuthWrapper()),
-                              (r) => false);
-                    },
-                  ),
-                ],
+                    const SizedBox(height: 5.0),
+                    FlatButton(
+                      child: Text(
+                        'Sign out',
+                        style: Theme
+                            .of(context)
+                            .textTheme
+                            .subtitle2
+                            .copyWith(
+                          color: Colors.cyan[400],
+                          fontSize: 15.0,
+                        ),
+                      ),
+                      onPressed: () async {
+                        await _auth.signOut();
+                        Navigator.of(context).pushAndRemoveUntil(
+                            MaterialPageRoute(builder: (_) => AuthWrapper()),
+                                (r) => false);
+                      },
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          ListTile(
-            title: const Text('Search'),
-            leading: const Icon(
-              Icons.search,
+            ListTile(
+              title: const Text('Search'),
+              leading: const Icon(
+                Icons.search,
+              ),
+              onTap: () {
+                Navigator.of(context).pop();
+                drawerNotifier.updateScreen('Search');
+              },
             ),
-            onTap: () {
-              Navigator.of(context).pop();
-              drawerNotifier.updateScreen('Search');
-            },
-          ),
-          ListTile(
-            title: const Text('My collection'),
-            leading: Image.asset(
-              'assets/images/yeezy.png',
-              width: 35.0,
+            ListTile(
+              title: const Text('My collection'),
+              leading: Image.asset(
+                'assets/images/yeezy.png',
+                width: 35.0,
+              ),
+              onTap: () {
+                Navigator.of(context).pop();
+                drawerNotifier.updateScreen('Collection');
+              },
             ),
-            onTap: () {
-              Navigator.of(context).pop();
-              drawerNotifier.updateScreen('Collection');
-            },
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
